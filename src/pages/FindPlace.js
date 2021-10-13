eea0588f6d533ad3c7c499af8da29efabe93a2dc
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-10-12 21:13:37 -0700

    working on location

--- src/pages/FindPlace.js
+++ src/pages/FindPlace.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function FindPlace() {
-	let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${REACT_GG_KEY}`
 
-	return <p>search page</p>
+	const [latitude, setLatitude] = useState(null);
+	const [longitude, setLongitude] = useState(null);
+	const [found, setFound] = useState(false);
+	const [coord, setCoord] = useState(false);
+
+	const getLocation = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(getCoordinates);
+		} else {
+			alert("Geolocation is not supported in this browsers");
+		}
+	}
+
+	const getCoordinates = position => {
+		setLongitude(position.coords.longitude);
+    	setLatitude(position.coords.latitude);
+	}
+
+	useEffect(() => {
+		if (!coord) {
+			getLocation()
+			setCoord(true)
+		}
+		console.log(latitude, longitude)
+		// if (coord == true && !found && latitude != null && longitude != null) {
+		// 	fetch(url)
+		// 	.then(res => res.json())
+		// 	.then(data => {
+		// 		console.log(data)
+		// 		setFound(true)
+
+		// 	})
+		// 	.catch(err => console.log(err))
+		// }
+	}, [coord, found, latitude, longitude])
+
+	return <p></p>
 }
