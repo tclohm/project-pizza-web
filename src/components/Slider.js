c40eac24bbc2ad0dfcf9dab950ad35676eafd37f
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-11-01 21:13:26 -0700

    slider and taste meter

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 
 
-export default function Slider ({ name, value, set }) {
+export default function Slider ({ name, value, set, state }) {
 	const [v, setV] = useState(value)
 
 	const onChange = (e) => {
 		e.preventDefault()
 		setV(e.target.value)
-		set(e.target.value)
+		set({ 
+			...state,
+			[e.target.name]: Number(e.target.value)
+				
+		})
 	}
 
 	return (
 		<div className="flex flex-col">
 			<label className="text-xs text-gray-500">{name}</label>
-		    <input className="slider" type="range" name="sweet" value={v} min="0" max="5" step="0.1" onChange={e => onChange(e)} />
+		    <input className="slider" type="range" name={name.toLowerCase()} value={v} min="0" max="5" step="0.1" onChange={e => onChange(e)} />
 	    </div>
     );
 };
--- src/pages/TasteMeter.js
+++ src/pages/TasteMeter.js
@@ -5,26 +5,26 @@ import { PizzaInputContext } from "../context/PizzaInputContext";
 import Slider from "../components/Slider";
 
 export default function TasteMeter () {
+	const [image, setImage] = useState('')
+	const [taste, setTaste] = useState({
 
-	const [cheeseSize, setCheeseSize] = useState(0)
-	const [flavorSize, setFlavorSize] = useState(0)
-	const [saucinessSize, setSaucinessSize] = useState(0)
-	const [saltinessSize, setSaltinessSize] = useState(0)
-	const [charSize, setCharSize] = useState(0)
+		cheesiness: 0,
+		flavor: 0,
+		sauciness: 0,
+		saltiness: 0,
+		charness: 0
+
+	})
 
 	const { input, add } = useContext(PizzaInputContext);
 
 	useEffect(() => {
-		add({
-			taste: {
-				cheesiness: Number(cheeseSize),
-				flavor: Number(flavorSize),
-				sauciness: Number(saucinessSize),
-				saltiness: Number(saltinessSize),
-				charness: Number(charSize),
-			}
+		fetch(`http://localhost:8000/image/${input.imageId}`)
+		.then(res => {
+			setImage(res.url)
 		})
-	}, [cheeseSize, flavorSize, saucinessSize, saltinessSize, charSize])
+		.catch(err => console.error(err))
+	}, [])
 
 	return (
 		<div className="absolute min-h-90 w-full">
@@ -33,44 +33,50 @@ export default function TasteMeter () {
 					<div className="flex lg:flex-col h-48 lg:min-h-90 lg:w-1/2 justify-center items-center">
 						<label
 							 name="Cheesiness"
-							 style={{ fontSize: `${cheeseSize}rem` }}
+							 style={{ fontSize: `${taste.cheesiness}rem` }}
 							 className="h-8 w-8 m-6 lg:m-8 flex items-center justify-around"
 							 >🧀</label>
 						<label
 							 name="Flavor"
-							 style={{ fontSize: `${flavorSize}rem` }}
+							 style={{ fontSize: `${taste.flavor}rem` }}
 							 className="h-8 w-8 m-6 lg:m-8 flex items-center justify-around"
 							 >👅</label>
 						<label
 							 name="Sauciness"
-							 style={{ fontSize: `${saucinessSize}rem` }}
+							 style={{ fontSize: `${taste.sauciness}rem` }}
 							 className="h-8 w-8 m-6 lg:m-8 flex items-center justify-around"
 							 >🍅</label>
 						<label
 							 name="Saltiness"
-							 style={{ fontSize: `${saltinessSize}rem` }}
+							 style={{ fontSize: `${taste.saltiness}rem` }}
 							 className="h-8 w-8 m-6 lg:m-8 flex items-center justify-around"
 							 >🧂</label>
 						<label
 							 name="Char"
-							 style={{ fontSize: `${charSize}rem` }}
+							 style={{ fontSize: `${taste.charness}rem` }}
 							 className="h-8 w-8 m-6 lg:m-8 flex items-center justify-around"
 						>🔥</label>
 					</div>
-					<div className="flex flex-col sm:mx-28 mx-12 lg:w-1/2 lg:justify-center">
+					<div className="flex flex-col sm:mx-28 mx-12 lg:w-1/2 lg:justify-center lg:mb-20">
 						<label className="text-lg font-black py-2 text-yellow-500">Taste</label>
-						<Slider name="Cheesiness" value={cheeseSize} set={setCheeseSize} />
-						<Slider name="Flavor" value={flavorSize} set={setFlavorSize} />
-						<Slider name="Sauciness" value={saucinessSize} set={setSaucinessSize} />
-						<Slider name="Saltiness" value={saltinessSize} set={setSaltinessSize} />
-						<Slider name="Char" value={charSize} set={setCharSize} />
+						<Slider name="Cheesiness" value={taste.cheesiness} set={setTaste} state={taste} />
+						<Slider name="Flavor" value={taste.flavor} set={setTaste} state={taste} />
+						<Slider name="Sauciness" value={taste.sauciness} set={setTaste} state={taste} />
+						<Slider name="Saltiness" value={taste.saltiness} set={setTaste} state={taste} />
+						<Slider name="Charness" value={taste.charness} set={setTaste} state={taste} />
 					</div>
 				</div>
 			</div>
 			<div className="absolute w-full flex justify-between bottom-2">
-				<div>
-					<p>pizza image</p>
-					<p>{input.pizzaName}</p>
+				<div className="flex ml-2 mb-2">
+					<img 
+					src={image} 
+					alt="food"
+					className="lg:h-20 lg:w-20 h-16 w-16 rounded" />
+					<div className="flex flex-col ml-2">
+						<p className="text-lg font-bold">{input.name}</p>
+						<p className="text-gray-600">{input.location.name}</p>
+					</div>
 				</div>
 				<button className="upload px-4 m-4 py-2 rounded-lg">submit</button>
 			</div>
