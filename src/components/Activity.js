fd5ef212e1b560bc00bd6c0a511d47c9e0c16f01
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-10-10 11:39:59 -0700

    contribution graph

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,7 +1,42 @@
 export default function Activity() {
+
+	const squares = document.querySelector('.squares');
+	for (var i = 1; i < 365; i++) {
+	  const level = Math.floor(Math.random() * 3);  
+	  squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
+	}
+
+	
 	return (
-		<div className="bg-red-200 h-1/2">
-			<p>Activity</p>
+		<div className="bg-red-200 h-2/5 font-black">
+			<h2>Activity</h2>
+			<div className="graph">
+				<ul className="months">
+				<li>Jan</li>
+				<li>Feb</li>
+				<li>Mar</li>
+				<li>Apr</li>
+				<li>May</li>
+				<li>Jun</li>
+				<li>Jul</li>
+				<li>Aug</li>
+				<li>Sep</li>
+				<li>Oct</li>
+				<li>Nov</li>
+				<li>Dec</li>
+				</ul>
+				<ul className="days">
+				<li>Sun</li>
+				<li>Mon</li>
+				<li>Tue</li>
+				<li>Wed</li>
+				<li>Thu</li>
+				<li>Fri</li>
+				<li>Sat</li>
+				</ul>
+				<ul className="squares">
+				</ul>
+			</div>
 		</div>
 	)
 }
