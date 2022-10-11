d52852b34d3b68e1c38d508dfed61205b203e3b9
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-10-10 20:03:19 -0700

    calendar

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,7 @@ import Activity from "../components/Activity";
 
 export default function Profile() {
 	return (
-		<div className="absolute flex h-5/6 w-screen">
+		<div className="absolute flex flex-col md:flex-row h-5/6 w-screen">
 			<Sidebar/>
 			<div className="flex flex-col justify-around w-full">
 				<Favorites />
